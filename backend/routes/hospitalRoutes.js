71e263d762534a310d8cf46b586eaef3d766cf27
Rupak <rupakt525@gmail.com>
2021-06-24 19:11:12 +0545

    Added Hospital Profile Update/ Services

--- backend/routes/hospitalRoutes.js
+++ backend/routes/hospitalRoutes.js
@@ -1,7 +1,12 @@
 
 const route= require('express').Router()
-const {registerHospital, loginHospital, individualHospital}= require('../controllers/hospitalControllers')
+const {registerHospital, loginHospital, individualHospital, upateHospital, addService}= require('../controllers/hospitalControllers')
+
 route.post('/registerHospital', registerHospital)
 route.post('/loginHospital', loginHospital)
 route.get('/:id', individualHospital)
+route.put('/:id/hospitalProfile', upateHospital)
+route.post('/:id/services/addservice', addService)
+
+
 module.exports = route;
