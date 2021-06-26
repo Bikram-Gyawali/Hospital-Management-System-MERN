f85b2a296e20468a9ac2671db73bb9165178eeae
Bikram Gyawali <bikramgyawali57@gmail.com>
2021-06-26 14:10:13 +0545

    imamge file upload with multer on backend done

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDb = require("./config/db");
+const path = require("path");
+const multer = require("multer");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
 
+// routes
 const hospitalRoutes = require("./routes/hospitalRoutes");
 const userRoutes = require("./routes/userRoutes");
 
@@ -14,7 +19,29 @@ dotenv.config();
 //middlewares
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
 app.use(express.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join(__dirname, "/public/images")));
+
+// multer implementation for uploading files/images
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File Uploaded Successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.use("/api/hospitals", hospitalRoutes);
 app.use("/api/user", userRoutes);
