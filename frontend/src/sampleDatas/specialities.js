5911dece1c1379b2c3c192fe8f7b9d2428611494
AyushMainali123 <ayushmainali11@gmail.com>
2021-07-01 23:19:05 +0545

    added sampleDatas to deliver data to components smoothly

---
+++ frontend/src/sampleDatas/specialities.js
@@ -0,0 +1,47 @@
+import PrimaryCare from 'assets/images/primary-care.svg'
+import Pediatrics from 'assets/images/pediatrics.svg'
+import Furnished from 'assets/images/furnished.svg'
+import PhysicalTherapy from 'assets/images/physical-therapy.svg'
+import EyeCare from 'assets/images/eye-care.svg'
+import DentalSurgery from 'assets/images/dental-surgery.svg'
+
+const data = [
+    {
+        id: 1,
+        imageUrl: PrimaryCare,
+        title: "Primary Care",
+        description: "Professional solution in house experts"
+    },
+    {
+        id: 2,
+        imageUrl: Pediatrics,
+        title: "Pediatrics",
+        description: "Well rounded care for your babies."
+    },
+    {
+        id: 3,
+        imageUrl: Furnished,
+        title: "Furnished",
+        description: "Get consulted online with professional health workers."
+    },
+    {
+        id: 4,
+        imageUrl: PhysicalTherapy,
+        title: "Physical Therapy",
+        description: "Great therapy by the doctors and clinics"
+    },
+    {
+        id: 5,
+        imageUrl: EyeCare,
+        title: "Eye Care",
+        description: "Professional ophthalmologist who cares your vision "
+    },
+    {
+        id: 6,
+        imageUrl: DentalSurgery,
+        title: "Dental Surgery",
+        description: "Guidence by cerified dentists."
+    },
+]
+
+export default data;
