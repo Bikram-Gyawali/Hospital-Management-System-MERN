7d12fa14843b752702f050ae8f1cfc5e9a9c9619
AyushMainali123 <ayushmainali11@gmail.com>
2021-07-01 23:13:19 +0545

    Added new routes and styles change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,29 @@ import HomeScreen from './screens/HomeScreen'
 import AboutScreen from './screens/AboutScreen'
 import ServiceScreen from './screens/ServiceScreen'
 import DoctorsScreen from './screens/DoctorsScreen'
+import CovidScreen from './screens/CovidScreen'
 import './App.css';
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+  max-width: 1600px;
+  margin: auto;
+`
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={ HomeScreen }/>
-      <Route exact path="/about" component={ AboutScreen }/>
-      <Route exact path="/services" component={ ServiceScreen }/>
-      <Route exact path="/doctors" component={ DoctorsScreen }/>
-      <Route exact path="/login" component={LoginScreen} />
-      <Route exact path="/register" component={RegisterScreen} />
-    </Router>
+    <AppContainer>
+      <Router>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/about" component={AboutScreen} />
+        <Route exact path="/services" component={ServiceScreen} />
+        <Route exact path="/doctors" component={DoctorsScreen} />
+        <Route exact path="/login" component={LoginScreen} />
+        <Route exact path="/register" component={RegisterScreen} />
+        <Route exact path="/covid19" component={CovidScreen} />
+      </Router>
+    </AppContainer>
+
   );
 }
 
