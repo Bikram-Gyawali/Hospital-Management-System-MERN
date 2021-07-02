e6dae9b307acd0f62eec8cc32eac131a0a75174e
Bikram Gyawali <bikramgyawali57@gmail.com>
2021-07-02 08:37:27 +0545

    conflicting management is difficult work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,15 @@
-// <<<<<<< HEAD
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {Provider} from 'react-redux';
-import ReactDOM from 'react-dom';
-import store from './store'
-
 import reportWebVitals from "./reportWebVitals";
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// =======
-
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>,document.getElementById('root')
-  </React.StrictMode>
-// >>>>>>> origin/master
+  </Provider>,
+  document.getElementById("root")
 );
 reportWebVitals();
