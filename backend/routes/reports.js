428a701f45e5ddf850fb92fa644375a9211a67b0
Rupak <rupakt525@gmail.com>
2021-07-04 00:38:48 +0545

    Report add and download

---
+++ backend/routes/reports.js
@@ -0,0 +1,22 @@
+const route = require('express').Router()
+const { v4: uuidv4 } = require('uuid');
+const path= require('path');
+
+const {saveReports, downloadReport}=require('../controllers/reportControllers')
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/reports')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}_${Date.now()}${uuidv4()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+route.post('/:id/uploadreport', upload.single('report'), saveReports)
+route.get('/:id/downloadreport', downloadReport)
+
+module.exports= route
